e7bd2f2d5128a64e5879332b457ffef89ac655c9
codefreak <harshsaid558@gmail.com>
2022-05-26 22:41:26 +0530

    changes

--- src/com/maddi/Multithreading.java
+++ src/com/maddi/Multithreading.java
@@ -1,6 +1,34 @@
 package com.maddi;
 
+//Creating thread using class
+//class Thread1 extends Thread {
+//    @Override
+//    public void run() {
+//        System.out.println("I am Thread 1");
+//    }
+//}
+//public class Multithreading  {
+//    public static void main(String[] args) {
+//        Thread1 t1 = new Thread1();
+//        t1.start();
+//    }
+//}
 
-public class Multithreading  {
+//creating thread using Runnable Interface
 
+//class Thread1 implements Runnable {
+//    @Override
+//    public void run() {
+//        System.out.println("I am Runnable");
+//    }
+//
+//}
+//
+//public class Multithreading {
+//    public static void main(String[] args) {
+//        Thread1 t1 = new Thread1();
+//
+//        Thread obj = new Thread(t1);
+//        obj.start();
+//    }
 }
